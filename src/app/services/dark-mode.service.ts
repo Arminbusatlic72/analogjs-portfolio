9bb33864a8b15019fed9a9b0a87a8be4314ba829
Armin <busatar@cronos.be>
2024-08-12 17:44:06 +0200

    fixing dark mode toggle

--- src/app/services/dark-mode.service.ts
+++ src/app/services/dark-mode.service.ts
@@ -43,18 +43,21 @@ import { Inject, PLATFORM_ID } from '@angular/core';
   providedIn: 'root',
 })
 export class DarkModeService {
-  darkModeSignal = signal<string>(this.getInitialDarkMode());
+  darkModeSignal = signal<string>(
+    JSON.parse(window.localStorage.getItem('darModeSignal') ?? 'null')
+  );
 
   updateDarkMode() {
-    this.darkModeSignal.update((value) =>
-      value === 'dark' ? 'light' : 'dark'
-    );
+    this.darkModeSignal.update((value) => (value === 'dark' ? 'null' : 'dark'));
   }
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
       effect(() => {
-        window.localStorage.setItem('darkModeSignal', this.darkModeSignal());
+        window.localStorage.setItem(
+          'darkModeSignal',
+          JSON.stringify(this.darkModeSignal())
+        );
       });
     }
   }
