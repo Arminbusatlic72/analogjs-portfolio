78ad06f8cf7a242ec1fe5c60febe6300ea28dcf3
Armin <busatar@cronos.be>
2024-08-12 17:09:32 +0200

    fixing local storage issue

--- src/app/services/dark-mode.service.ts
+++ src/app/services/dark-mode.service.ts
@@ -1,3 +1,40 @@
+// import { effect, Injectable, signal } from '@angular/core';
+// import { isPlatformBrowser } from '@angular/common';
+// import { Inject, PLATFORM_ID } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class DarkModeService {
+//   darkModeSignal = signal<string>(this.getInitialDarkMode());
+
+//   updateDarkMode() {
+//     this.darkModeSignal.update((value) =>
+//       value === 'dark' ? 'light' : 'dark'
+//     );
+//   }
+
+//   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+//     if (isPlatformBrowser(this.platformId)) {
+//       effect(() => {
+//         window.localStorage.setItem(
+//           'darkModeSignal',
+//           JSON.stringify(this.darkModeSignal())
+//         );
+//       });
+//     }
+//   }
+
+//   private getInitialDarkMode(): string {
+//     if (isPlatformBrowser(this.platformId)) {
+//       return JSON.parse(
+//         window.localStorage.getItem('darkModeSignal') ?? 'light'
+//       );
+//     }
+//     return 'light';
+//   }
+// }
+
 import { effect, Injectable, signal } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, PLATFORM_ID } from '@angular/core';
@@ -17,19 +54,17 @@ export class DarkModeService {
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
       effect(() => {
-        window.localStorage.setItem(
-          'darkModeSignal',
-          JSON.stringify(this.darkModeSignal())
-        );
+        window.localStorage.setItem('darkModeSignal', this.darkModeSignal());
       });
     }
   }
 
   private getInitialDarkMode(): string {
     if (isPlatformBrowser(this.platformId)) {
-      return JSON.parse(
-        window.localStorage.getItem('darkModeSignal') ?? 'light'
-      );
+      const storedMode = window.localStorage.getItem('darkModeSignal');
+      return storedMode === 'dark' || storedMode === 'light'
+        ? storedMode
+        : 'light';
     }
     return 'light';
   }
