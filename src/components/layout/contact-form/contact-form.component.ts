aa8298bb191792859fb9cc648ad9ba643eb8a85a
Armin <busatar@cronos.be>
2024-09-06 11:43:13 +0200

    fixing contact form

--- src/components/layout/contact-form/contact-form.component.ts
+++ src/components/layout/contact-form/contact-form.component.ts
@@ -13,9 +13,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
     method="POST"
     data-netlify-recaptcha="true"
     data-netlify="true"
+    data-netlify-honeypot="bot-field"
     class="flex flex-wrap -m-2"
     netlify
   >
+    <input type="hidden" name="bot-field" />
     <input type="hidden" name="form-name" value="contact-form" />
     <div class="p-2 w-1/2">
       <div class="relative">
@@ -101,7 +103,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
         Submit message
       </button>
     </div>
-    <div data-netlify-recaptcha="true"></div>
+    <div class="p2" data-netlify-recaptcha="true"></div>
   </form>`,
 })
 export class ContactFormComponent {
